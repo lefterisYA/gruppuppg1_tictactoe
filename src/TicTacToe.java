784ced2209eedb6c82974e623f45da7c123e8136
lefterisYA <elefterios.lundell@yaelev.se>
2021-10-30 11:29:10 +0200

    new start with anas

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -8,10 +8,12 @@ public class TicTacToe {
         logic.play(sc);
         sc.close();
     }
-
 }
 
 class Logic {
+	// 0 betyder: ledig ruta
+	// 1 betyder: rutan till spelare 1
+	// 2 betyder: rutan till spelare 2
     int[][] board;
 
     public Logic(int gridSize) {
@@ -21,24 +23,32 @@ class Logic {
     public void play(Scanner sc) {
         UI.draw(board);
 
-        int player = 2;
+        int currPlayer = 2;
         int playCnt = 0;
-        while ( ! WinChecker.check(board, player, playCnt) ) {
+        while ( ! WinChecker.check(board, currPlayer, playCnt) ) {
         	playCnt++;
-            player = nextPlayer(player);
+            currPlayer = nextPlayer(currPlayer);
 
-            System.out.println("Input 1-9:");
+            System.out.println("Player " + 
+            		currPlayer + " it's your turn. Input 1-9:");
+
             int tile = sc.nextInt();
+            int[] coord = sequentialToCoordinates(tile);
 
-            int y = (tile-1) / 3;
-            int x = (tile-1) % 3;
-
-            board[y][x] = player;
+            board[coord[0]][coord[1]] = currPlayer;
 
             UI.draw(board);
         }
 
-        System.out.println("Congratulations player " + player + "! You won!!");
+        System.out.println("Congratulations player " + currPlayer + "! You won!!");
+    }
+    
+    private int[] sequentialToCoordinates(int tile) {
+    	int[] ret = new int[2];
+    	ret[0] = (tile-1) / 3;
+    	ret[1] = (tile-1) % 3;
+    	
+    	return ret;
     }
 
     // Just return the opposite:
@@ -51,29 +61,39 @@ class Logic {
  * Should draw the board given a 2D array.
  */
 class UI {
-    public static void draw(int[][]board) {
-		System.out.println("-"+"--".repeat(board.length));
+	public static void draw(int[][] board) {
+		drawUi(board);
+		drawDebug(board);
+	}
+	
+    public static void drawDebug(int[][] board) {
 		for ( int y = 0; y<board.length; y++ ) {
-			System.out.print("|");
 			for ( int x = 0; x<board.length; x++ ) {
-				String tile = board[y][x] == 1 ? "X" : board[y][x] == 2 ? "O" : " "; 
-				System.out.print(tile+"|");
+				System.out.print(board[y][x]);
 			}
-			System.out.println("\n-"+"--".repeat(board.length));
+			System.out.println("");
 		}
     }
+	public static void drawUi(int[][]board) {
+		System.out.println("");
+	}
 }
 
 /*
  * Should check whether any player has won by checking if there are three of the same boxes in the 2D array.
  */
 class WinChecker {
+	/* Check takes a 2D-array and returns true if there exists a straight
+	 * line with all values equal and not zero.
+	 */
     public static boolean check(int[][] board, int currPlayer, int playCnt) {
       // TODO
     	return false;
     }
 
-    // By transforming the board we get can easier iterate over the elements and so check the vertical lines too.
+    /*
+     * By transforming the board we get can easier iterate over the elements and so check the vertical lines too.
+     */
     private static int[][] rotateBoard(int[][] boardIn) {
       // TODO
     	return null;
@@ -84,4 +104,9 @@ class WinChecker {
         //TODO
     	return false;
     }
+    
+	private static int[][] genDiaLines(int[][] boardIn){
+
+		return null;
+	}
 }
